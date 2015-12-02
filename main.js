ae6453ecaff6a9a5a6cccd8d191b4f47700be1c3
Nikhil Chinthapallee <nchinth@ncsu.edu>
2015-12-02 15:16:05 -0500

    Slight Modifications

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ var express = require('express')
 var fs      = require('fs')
 var app = express()
 
-var args = process.argv.slice(2);
-var PORT = args[0];
+
+
 
 app.get('/', function(req, res) 
 {
 	res.send("Hello from production !!!");
 });
 
-var server = app.listen(PORT, function () {
+var server = app.listen(3000, function () {
 
 	var host = server.address().address
 	var port = server.address().port
